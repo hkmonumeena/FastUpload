c2d22446a50bf9f0e21d95065c633533dde2770c
Sir <monum811@gmail.com>
2021-01-10 21:50:23 +0530

    Activate Library

--- sample/src/main/java/com/picker/example/MainActivity.kt
+++ sample/src/main/java/com/picker/example/MainActivity.kt
@@ -5,10 +5,7 @@ import android.content.Intent
 import android.location.Address
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.smartlib.quickimage.AddressPickerActivity
-import com.smartlib.quickimage.AddressPickerActivity.Companion.RESULT_ADDRESS
-import com.smartlib.quickimage.MyLatLng
-import com.smartlib.quickimage.Pin
+
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -21,26 +18,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         get_address?.setOnClickListener {
-            val intent = Intent(this@MainActivity, AddressPickerActivity::class.java)
-            intent.putExtra(AddressPickerActivity.ARG_LAT_LNG,MyLatLng(42.5328966, -122.7751082))
-            val pinList=ArrayList<Pin>()
-            pinList.add(Pin(MyLatLng(42.329989, -122.3100),"Work"))
-            pinList.add(Pin(MyLatLng(42.023123, -122.23414),"Home"))
-            intent.putExtra(AddressPickerActivity.ARG_LIST_PIN,  pinList)
-            intent.putExtra(AddressPickerActivity.ARG_ZOOM_LEVEL,  1.0f)
-            startActivityForResult(
-                intent,
-                REQUEST_ADDRESS
-            )
+
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == REQUEST_ADDRESS && resultCode == Activity.RESULT_OK) {
-            val address: Address? = data?.getParcelableExtra(RESULT_ADDRESS) as Address
-            selected_address.text =
-                address?.featureName + ", " + address?.locality + ", " + address?.adminArea + ", " + address?.countryName
+
+           
 
         }
     }
